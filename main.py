5664faa808a0d2c99a8716213d0e4127260fec4c
Soham Pal <sohampal101@gmail.com>
2021-07-27 20:34:45 +0530

    UPD: cchanges in model

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import joblib
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from fastapi.middleware.cors import CORSMiddleware
-
-dataframe = pd.read_csv("dataset/spam.csv")
-
-cv = CountVectorizer()
-features = cv.fit_transform(dataframe["EmailText"][0:4457])
-model = joblib.load('model/spam_classifier')
+from train import clean_data
 
 app = FastAPI()
 origins = ["*"]
@@ -21,7 +16,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+loaded_model = joblib.load("model/spam_classifier")
+loaded_cvec = joblib.load("model/count_vectorizer")
+loaded_tfidf_transformer = joblib.load("model/tfidf_transformer")
+
 @app.get("/{content}")
 def get_prediction(content: str):
-    return model.predict(cv.transform([content]))[0]
+    [content] = clean_data({"message": content})
+    return loaded_model.predict(
+        loaded_tfidf_transformer.transform(
+            loaded_cvec.transform(
+                [content]
+            )
+        )
+    )[0]
     
